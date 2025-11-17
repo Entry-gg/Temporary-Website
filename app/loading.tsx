26573b20351644236636db169c22047d1cf92f11
Jellyfish <ElliotLueck@outlook.com>
2025-11-17 09:29:23 -0600

    Create loading.tsx

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <div className="h-15 w-15 animate-spin rounded-full border-5 border-entry-main border-t-transparent" />
+    </div>
+  );
+}
