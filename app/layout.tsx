92a7d5cda735445c2d55c715c8bb37c4c8fc2187
Jrllyfish <jellyfish@entry.gg>
2025-11-20 21:56:47 -0600

    Add customized metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,22 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Entry.gg - Coming Soon",
   description: "Entry.gg is the ultimate tool to create, deploy, and manage forms & applications for your community.",
+  openGraph: {
+    title: "Entry.gg",
+    description: "Entry is the ultimate tool for creating and deploying forms, applications, and much more!",
+    url: "https://entry.gg",
+    siteName: "Entry.gg",
+    images: [
+      {
+        url: "https://entry.gg/Entry.png",
+        alt: "Entry.gg Logo",
+        width: 800,
+        height: 800
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  }
 };
 
 export default function RootLayout({
